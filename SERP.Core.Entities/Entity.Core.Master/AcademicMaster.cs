963033eb95e9ff7742f624de078e1027ea09c928
Manoj Sharma <manojsharma00018@gmail.com>
2019-12-19 22:41:47 -0500

    Arcitect Created

--- SERP.Core.Entities/Entity.Core.Master/AcademicMaster.cs
+++ SERP.Core.Entities/Entity.Core.Master/AcademicMaster.cs
@@ -2,14 +2,18 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
+using System.ComponentModel.DataAnnotations;
 
 namespace SERP.Core.Entities.Entity.Core.Master
 {
-    [Table("AcademicMaster",Schema = "Master")]
-    public class AcademicMaster:Base<int>
+    [Table("AcademicMaster", Schema = "Master")]
+    public class AcademicMaster : Base<int>
     {
+        [Required]
         public string Name { get; set; }
+        [Required]
         public DateTime StartDate { get; set; }
+        [Required]
         public DateTime EndDate { get; set; }
         public string Description { get; set; }
     }
---
+++ SERP.UI/Controllers/Master/AcademicMasterController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SERP.UI.Controllers.Master
+{
+    [Route("Master/{controller}/{action}")]
+    public class AcademicMasterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
