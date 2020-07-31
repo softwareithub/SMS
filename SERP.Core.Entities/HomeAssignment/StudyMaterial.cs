8e7f694a2fb38372f468de046c4fd85f34b7465c
Manoj-MCA <46224046+Manoj-MCA@users.noreply.github.com>
2020-08-01 02:53:28 +0530

    https://www.pivotaltracker.com/story/show/173998889
    https://www.pivotaltracker.com/story/show/173998889
    https://www.pivotaltracker.com/story/show/173998888
    https://www.pivotaltracker.com/story/show/173998876
    https://www.pivotaltracker.com/story/show/173998867
    https://www.pivotaltracker.com/story/show/174058601
    https://www.pivotaltracker.com/story/show/174058574
    https://www.pivotaltracker.com/story/show/174058565
    https://www.pivotaltracker.com/story/show/174110861
    https://www.pivotaltracker.com/story/show/174120764
    https://www.pivotaltracker.com/story/show/173998891

--- SERP.Core.Entities/HomeAssignment/StudyMaterial.cs
+++ SERP.Core.Entities/HomeAssignment/StudyMaterial.cs
@@ -10,15 +10,26 @@ namespace SERP.Core.Entities.HomeAssignment
     [Table("StudyMaterial", Schema = "Master")]
     public class StudyMaterial:Base<int>
     {
+        [Required(ErrorMessage ="Please Enter Title")]
         public string MaterialName { get; set; } = string.Empty;
         public string MaterialDescription { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Please Select Course/Class")]
         public int CourseId { get; set; } = default;
+
+        [Required(ErrorMessage = "Please Select Batch/Section")]
         public int BatchId { get; set; } = default;
+
+        [Required(ErrorMessage = "Please Select Subject")]
         public int SubjectId { get; set; } = default;
+
+        [Required(ErrorMessage = "Please Upload File")]
         public string MaterialPath { get; set; } = string.Empty;
 
         [DataType(DataType.Date)]
         public DateTime PublishDate { get; set; } = default;
+
+        [Required(ErrorMessage = "Please Select Upload Type")]
         public string UploadType { get; set; } = string.Empty;
 
     }
