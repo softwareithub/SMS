d75a0937ecc509b791cbc47a5786f6c76e37875e
Manoj-MCA <46224046+Manoj-MCA@users.noreply.github.com>
2020-08-18 17:48:14 +0530

    Added required fields to guardian page

--- SERP.Core.Entities/Entity.Core.Master/GuardianMaster.cs
+++ SERP.Core.Entities/Entity.Core.Master/GuardianMaster.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
@@ -8,11 +9,22 @@ namespace SERP.Core.Entities.Entity.Core.Master
     [Table("GuradianMaster")]
     public class GuardianMaster : Base<int>
     {
+        [Required(ErrorMessage = "Please select student")]
         public int StudentId { get; set; } = default;
+
+        [Required(ErrorMessage = "Please enter guardian name")]
         public string GuradianName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Please select relationship")]
         public string RelationShip { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Please enter phone number")]
         public string Phone { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Please enter email")]
         public string Email { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Please upload guardian picture")]
         public string GuardianImage { get; set; } = string.Empty;
         public int IsVerified { get; set; } = default;
         [NotMapped]
