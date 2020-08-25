c02b767739fb9fe923e633d9d0d34af12b7028f8
Manoj-MCA <46224046+Manoj-MCA@users.noreply.github.com>
2020-08-25 15:25:07 +0530

    Fixed issue for lesson topic

--- SERP.Core.Entities/HomeAssignment/LessonTopicMapping.cs
+++ SERP.Core.Entities/HomeAssignment/LessonTopicMapping.cs
@@ -10,13 +10,16 @@ namespace SERP.Core.Entities.HomeAssignment
     [Table("LessonTopicMapping", Schema ="Master")]
     public class LessonTopicMapping :Base<int>
     {
+        [Required(ErrorMessage ="Please Select Lesson")]
         public int LessonId { get; set; } = default;
+        [Required(ErrorMessage ="Please Enter Topic Name")]
         public string TopicName { get; set; } = default;
 
         [DataType(DataType.Date)]
         public DateTime? StartDate { get; set; } = default;
         [DataType(DataType.Date)]
         public DateTime? EndDate { get; set; } = default;
+        [Required(ErrorMessage ="Please Enter Topic Description")]
         public string TopicDescription { get; set; } = string.Empty;
         public int ExpectedClass { get; set; } = default;
         public int SessionId { get; set; }
